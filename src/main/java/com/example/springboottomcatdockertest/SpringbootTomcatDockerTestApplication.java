826e84ca2d8070d82b9a5a6183c393d18c504974
cruway <astralkaiser89@gmail.com>
2023-04-29 22:03:52 +0900

    init

---
+++ src/main/java/com/example/springboottomcatdockertest/SpringbootTomcatDockerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottomcatdockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTomcatDockerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTomcatDockerTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboottomcatdockertest/SpringbootTomcatDockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottomcatdockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTomcatDockerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
